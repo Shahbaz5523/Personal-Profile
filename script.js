64b7a0427fa3efb364fbe2f03c21bf1fd8cd8f54
Shahbaz5523 <shahbaz.rajpoot15@gmail.com>
2019-10-31 13:37:31 +0500

    ProgressBar animation

---
+++ script.js
@@ -0,0 +1,17 @@
+let progress = document.querySelector("progress");
+let progressValue = progress.value;
+
+
+
+
+
+
+
+
+// some.classList.add('animation');
+
+// some.onmouseout = function(){
+// some.classList.remove('animation');
+    
+// }
+
